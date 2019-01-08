bf0313392e0c967b9b2fa59bc06c7e7f4550ed59
Cliff <kurniawancliff@gmail.com>
2019-01-08 15:46:43 +1100

    fixed firstDiv in testimonial

--- jscript.js
+++ jscript.js
@@ -2,8 +2,9 @@ $(document).ready(function(){
   $("img").on('click',function(){
    
    var hello = $(this).attr('data-id');
-   $('.hideDiv').hide();
-   $('#'+hello).show();
+    $('.firstDiv').hide();
+    $('.hideDiv').hide();
+    $('#'+hello).show();
     
    $('#testimonialCompany').css('opacity', '1');
   });
